052bc288e991a32a8d759630138cb694ea4ff666
Dalgat Abdullabekov <abd.dalgat@gmail.com>
2023-08-14 18:52:53 +0300

    third

--- ViewModels/ListViewModel.swift
+++ ViewModels/ListViewModel.swift
@@ -32,6 +32,21 @@ class ListViewModel: ObservableObject {
         items.remove(atOffsets: indexSet)
     }
     
+    func moveItem(from: IndexSet, to: Int) {
+        items.move(fromOffsets: from, toOffset: to)
+    }
+    
+    func addNewItem(title: String, description: String) {
+        let newItem = ItemModel(title: title, description: description, isCompleted: false)
+        items.append(newItem)
+    }
+    
+    func updateItem(item: ItemModel) {
+        if let index = items.firstIndex(where: {$0.id == item.id}) {
+            items[index] = item.updateCompletion()
+        }
+    }
+    
     func saveItems() {
         if let encodedData = try? JSONEncoder().encode(items) {
             UserDefaults.standard.set(encodedData, forKey: itemsKey)
---
+++ Views/ListView.swift
@@ -0,0 +1,14 @@
+//
+//  ListView.swift
+//  To-Do.List
+//
+//  Created by Abdullabekov Dalgat on 8/14/23.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        
+    }
+}
