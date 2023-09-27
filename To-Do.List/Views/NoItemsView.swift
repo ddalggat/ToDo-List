3762df81a91bf7dd0cd3306d65068bd12f6d124e
Dalgat Abdullabekov <abd.dalgat@gmail.com>
2023-09-27 22:53:33 +0300

    change noitems

--- To-Do.List/Views/NoItemsView.swift
+++ To-Do.List/Views/NoItemsView.swift
@@ -18,3 +18,25 @@ struct NoItemsView: View {
                 Text("There are no items!")
                     .font(.title)
                     .fontWeight(.semibold)
+                Text("Are you a productive person? I think you should click the add button and add a bunch of items to your todo list!")
+                    .padding(.bottom, 20)
+                
+                NavigationLink(
+                    destination: AddView(),
+                    label: {
+                        Text("Add Something 📋")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .frame(height: 55)
+                            .frame(maxWidth: .infinity)
+                            .background(animate ? secondaryAccentColor : Color.accentColor)
+                            .cornerRadius(10)
+                    })
+                    .padding(.horizontal, animate ? 30 : 50)
+                    .shadow(
+                        color: animate ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
+                        radius: animate ? 30 : 10,
+                        x: 0,
+                        y: animate ? 50 : 30)
+                    .scaleEffect(animate ? 1.1 : 1.0)
+                    .offset(y: animate ? -7 : 0)
