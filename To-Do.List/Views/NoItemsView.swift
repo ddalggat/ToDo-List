e9f0bb6b8ec301ff1d1567450b41fa27ae0177ab
Dalgat Abdullabekov <abd.dalgat@gmail.com>
2023-09-27 22:54:58 +0300

    pre final changes

--- To-Do.List/Views/NoItemsView.swift
+++ To-Do.List/Views/NoItemsView.swift
@@ -11,7 +11,7 @@ struct NoItemsView: View {
     
     @State var animate: Bool = false
     let secondaryAccentColor = Color("SecondaryAccentColor")
-
+    
     var body: some View {
         ScrollView {
             VStack(spacing: 10) {
@@ -32,11 +32,21 @@ struct NoItemsView: View {
                             .background(animate ? secondaryAccentColor : Color.accentColor)
                             .cornerRadius(10)
                     })
-                    .padding(.horizontal, animate ? 30 : 50)
-                    .shadow(
-                        color: animate ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
-                        radius: animate ? 30 : 10,
-                        x: 0,
-                        y: animate ? 50 : 30)
-                    .scaleEffect(animate ? 1.1 : 1.0)
-                    .offset(y: animate ? -7 : 0)
+                ..padding(.horizontal, animate ? 30 : 50)
+                .shadow(
+                    color: animate ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
+                    radius: animate ? 30 : 10,
+                    x: 0,
+                    y: animate ? 50 : 30)
+                .scaleEffect(animate ? 1.1 : 1.0)
+                .offset(y: animate ? -7 : 0)
+            }
+            .frame(maxWidth: 400)
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
+    
