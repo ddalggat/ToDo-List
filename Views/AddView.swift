507301d5adb33d62bb3353ed67cec7dbcf9311d7
Dalgat Abdullabekov <abd.dalgat@gmail.com>
2023-09-28 22:47:07 +0300

    add AddView

---
+++ Views/AddView.swift
@@ -0,0 +1,48 @@
+//
+//  AddView.swift
+//  To-Do.List
+//
+//  Created by Abdullabekov Dalgat on 9/28/23.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    
+    // MARK: PROPERTIES
+    
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listViewModel: ListViewModel
+    @State var textFieldText: String = ""
+    
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
+    
+    // MARK: BODY
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                    .background(Color(UIColor.secondarySystemBackground))
+                    .cornerRadius(10)
+                
+                Button(action: saveButtonPressed, label: {
+                    Text("Save".uppercased())
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                })
+            }
+            .padding(14)
+        }
+        .navigationTitle("Add an Item 🖊")
+        .alert(isPresented: $showAlert, content: getAlert)
+    }
+    
+
