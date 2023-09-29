3cbf00585b9e2eafab4ffd6b5a62b05994cd5dda
Dalgat Abdullabekov <abd.dalgat@gmail.com>
2023-09-29 19:13:13 +0300

    add list row

---
+++ Views/ListRowView.swift
@@ -0,0 +1,26 @@
+//
+//  ListRowView.swift
+//  To-Do.List
+//
+//  Created by Abdullabekov Dalgat on 9/30/23.
+//
+
+import SwiftUI
+
+struct ListRowView: View {
+    
+    let item: ItemModel
+    
+    var body: some View {
+        HStack {
+            Image(systemName: item.isCompleted ? "checkmark.circle" : "circle")
+                .foregroundColor(item.isCompleted ? .green : .red)
+            Text(item.title)
+            Spacer()
+        }
+        .font(.title2)
+        .padding(.vertical, 8)
+    }
+}
+
+
