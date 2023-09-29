e65c3b46f3868272f4be7b577ac69957652e9275
Dalgat Abdullabekov <abd.dalgat@gmail.com>
2023-09-29 19:13:36 +0300

    finish list row

--- Views/ListRowView.swift
+++ Views/ListRowView.swift
@@ -23,4 +23,17 @@ struct ListRowView: View {
     }
 }
 
-
+struct ListRowView_Previews: PreviewProvider {
+    
+    static var item1 = ItemModel(title: "First item!", isCompleted: false)
+    static var item2 = ItemModel(title: "Second Item.", isCompleted: true)
+    
+    static var previews: some View {
+        Group {
+            ListRowView(item: item1)
+            ListRowView(item: item2)
+        }
+        .previewLayout(.sizeThatFits)
+        
+    }
+}
